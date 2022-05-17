fd1a76f933d1d2edfd366b03e8e1f1d6e2686336
unknown <halong9c1@gmail.com>
2022-05-18 02:04:07 +0700

    repush circleCI to check email

--- test/DB_driven_checkpassword.test.js
+++ test/DB_driven_checkpassword.test.js
@@ -2,6 +2,6 @@ import userService from '../services/userServices.js';
 
 describe('check two password and one password does not be hashed', () => {
     test('check user password', () => {
-        expect(userService.checkPassword('superadmin','superadmin')).toBe(true)
+        expect(userService.checkPassword('superadmin','superadmin1')).toBe(true)
     })
 })
