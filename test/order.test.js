124727cf7d2d1cd21a0b56442a4c4b2b23072ff5
unknown <halong9c1@gmail.com>
2022-05-18 16:04:41 +0700

    b

--- test/order.test.js
+++ test/order.test.js
@@ -2,6 +2,9 @@ import orderService from '../services/orderServices.js';
 
 describe('OrderService', () => {
     test('order is empty', () => {
+
+
+        ///
         expect(orderService.checkEmptyOrder(0)).toBe(true)//DUNG
     });
 })
